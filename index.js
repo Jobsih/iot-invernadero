41355a2dd9d4d5b5d006fc88b13951a2c5ef91af
Jobsih <jorgeomarsih@gmail.com>
2023-12-03 15:40:56 -0600

    Tabla con historial de datos

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ var humTIdeal = 100;
 var humAIdeal = 100;
 var tempIdeal = 22;
 
+var prueba={
+"1701361701780" : {
+  "Humedad Ambiental": 50,
+"Humedad en Tierra": 60,
+"Temperatura" : 25,
+"fecha":"1701361701780"} ,
+"1701361781944":{
+  "Humedad Ambiental": 69,
+"Humedad en Tierra": 60,
+"Temperatura" : 25,
+"fecha":"1701361781944"} ,
+"1701362079682" :{
+  "Humedad Ambiental": 69,
+"Humedad en Tierra": 60,
+"Temperatura" : 25,
+"fecha":"1701362079682"} };
+
 // Puerto para la aplicación
 const port = process.env.PORT || 3000; // Utiliza el puerto proporcionado por fly.io o el puerto 3000 por defecto
 
@@ -51,7 +68,7 @@ app.post('/valor', async (req, res) => {
     // Referencia a la collecion que esta en firebase y crea un nuevo documento
     const fbSensores = db.collection('sensores').doc('registros').collection('valores').doc(Date.now().toString())
     // Ingresa los datos al documento
-    const fbRes = await fbSensores.set({'Temperatura': tempAct , 'Humedad Ambiental':humAAct, "Humedad en Tierra":humTAct})
+    const fbRes = await fbSensores.set({'Temperatura': tempAct , 'Humedad Ambiental':humAAct, "Humedad en Tierra":humTAct, 'Fecha': Date.now().toString()})
     //Si los valoes de temperatura o gas sobrepasan su limite entonces enciende el ventilador
     let mensaje = [0,0,0,0];
 
@@ -106,6 +123,53 @@ app.get('/historial', (req, res) => {
   res.sendFile(__dirname + '/view/historial.html');
 });
 
+app.get('/valores', async(req,res)=>{
+  /*db.collection('sensores').doc('registros').collection('valores')
+  .orderBy("",  'desc')
+  .onSnapshot((querySnapshot)=>{
+    const valores = {};
+    // Itera sobre los documentos y agrupa los datos en un objeto JSON
+    querySnapshot.forEach((doc) => {
+      valores[doc.id] = doc.data();
+    });
+    // Imprime el resultado en formato JSON
+    prueba = JSON.stringify(valores, null, 2);
+    }).catch((error) => {
+        console.error('Error al obtener datos:', error);
+    });*/
+    let elem10 = prueba.slice(0,10);
+    res.json(prueba)
+
+
+});
+
+app.post('/corregir', async (req, res) => {
+  db.collection('sensores').doc('registros').collection('valores').get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      // Actualiza el documento actual añadiendo el campo 'id' con el valor del ID del documento
+      db.collection('sensores').doc('registros').collection('valores').doc(doc.id).update({
+        fecha: doc.id
+      });
+    });
+  });
+  res.redirect('/historial');
+});
+
+function consigueValores(){
+  db.collection('sensores').doc('registros').collection('valores')
+  .orderBy("fecha", "desc").onSnapshot((querySnapshot)=>{
+    const valores = {};
+    // Itera sobre los documentos y agrupa los datos en un objeto JSON
+    querySnapshot.forEach((doc) => {
+      valores[doc.id] = doc.data();
+    });
+        // Imprime el resultado en formato JSON
+    prueba = JSON.stringify(valores, null, 2);
+    });
+    
+}
+
+
 app.get('/parametros', (req, res) => {
   res.sendFile(__dirname + '/view/parametros.html');
 });
@@ -113,12 +177,12 @@ app.get('/parametros', (req, res) => {
   // Manejo de conexiones WebSocket
   io.on('connection', (socket) => {
     console.log('Cliente conectado');
-  
+    consigueValores();  
     // Envía datos iniciales al cliente cuando se conecta
-    socket.emit('data', { humAAct, humTAct, tempAct, humAIdeal, humTIdeal, tempIdeal});
+    socket.emit('data', { humAAct, humTAct, tempAct, humAIdeal, humTIdeal, tempIdeal, prueba});
   });
 
   setInterval(() => {
     // Emite los nuevos valores a todos los clientes conectados
-    io.emit('data', { humAAct, humTAct, tempAct, humAIdeal, humTIdeal, tempIdeal});
+    io.emit('data', { humAAct, humTAct, tempAct, humAIdeal, humTIdeal, tempIdeal, prueba});
   }, 5000);  // Actualiza cada 5 segundos
